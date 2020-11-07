b5a15824beca03b1c9d0ffe5ea0e15136f34c661
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2020-11-07 13:20:55 +0600

    initial codes

---
+++ script.js
@@ -0,0 +1,8 @@
+const readMore = document.getElementById('read-more-content')
+const goAboutPage = document.getElementById('goaboutpage')
+
+
+function readMoreContent() {
+    // showing text 
+    readMore.innerHTML = '<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint blanditiis tempora minus, quod fugit doloribus sequi fugiat nesciunt repudiandae praesentium enim et iusto amet nulla impedit quidem! Voluptatum officiis praesentium ipsa quis vero laboriosam? Quasi adipisci voluptas sunt possimus molestiae alias! Provident maiores accusamus dolore praesentium deleniti dolor qui vitae.</p>'
+}
